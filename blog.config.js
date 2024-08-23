9782942b1542ef8aa7cd42d327438c14f2bed85e
鳌拜拜 <sep@aya.yale.edu>
2024-08-24 02:21:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,20 +20,20 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一名艺术者, Hi，我是一个搬砖人,Hi，我是一个终生学习的人,欢迎来到我的博客🎉',
+    'Hi，我是一名艺术工作者, Hi，我是一个搬砖人,Hi，我是一个终生学习的人,欢迎来到我的博客🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '团子', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个沉浮于红尘之中的人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://so.ikun.su/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ikun.su/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '学习, 博客, 记录, 生活', // 网站关键词 英文逗号隔开，
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'sep@aya.yale.edu', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
-  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
+  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/sepbigo', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/svip_user', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
